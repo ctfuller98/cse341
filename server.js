f9d9fcc51eedb49e361ce1040780af72f204af98
ctfuller98 <ful17005@byui.edu>
2022-04-29 13:06:12 -0600

    added contacts route with ugly db code

--- server.js
+++ server.js
@@ -5,19 +5,6 @@ const port = process.env.PORT || 3000;
 app.use('/', require('./routes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 }) 
 
-//Ugly Code
-const dotenv = require('dotenv');
-dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect(process.env.MONGODB_URI, function(err,db) {
-  if (err) throw err;
-  var dbo = db.db("contacts");
-  dbo.collection("contacts").find().toArray(function(err, result){
-    if (err) throw err;
-    console.log(result);
-    db.close();
-  });
-});
