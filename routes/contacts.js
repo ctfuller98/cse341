4f4989d10287413677e5104dfa39f9988d40bfae
ctfuller98 <ful17005@byui.edu>
2022-04-30 09:57:08 -0600

    added .rest endopoints

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,22 @@
 const routes = require('express').Router();
 const connect = require('../db/connect');
-
+const ObjectId = require('mongodb').ObjectId;
 routes.get('/', (req, res) => {
-
-   connect.getCollection().find().toArray((err, result) =>{
-      if (err) throw err;
-      res.json(result);
-      console.log("Contacts Query Successful");
-    });
+const results = connect.getCollection().find();
+results.toArray().then((documents)=> {
+  res.status(200).json(documents);
+  console.log('Returned All Contacts')
 });
+
+});
+
+routes.get('/:id', (req, res) => {
+  const contactid = new ObjectId(req.params.id)
+  const results = connect.getCollection().find({_id: contactid});
+  results.toArray().then((documents)=> {
+    res.status(200).json(documents[0]);
+    console.log(`Returned Contact ${req.params.id}`)
+  });
+  
+  });
   module.exports = routes;
