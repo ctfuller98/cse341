21ab8f972fff369ef32dc42af4b8f3316baf2aa8
ctfuller98 <ful17005@byui.edu>
2022-04-30 08:45:17 -0600

    cleaned up db code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const connect = require('./db/connect')
+connect.initDatabase();
 
 app.use('/', require('./routes'))
 
